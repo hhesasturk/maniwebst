ba2f18055fd4201189039a993a025d43b88affc7
Hüseyin Hilmi ESASTÜRK <hhesasturk@email.com>
2025-07-29 21:39:13 +0300

    Vercel konfigürasyonu basitleştirildi - site açılma sorunu çözüldü

--- maniwebst/api/manifest.js
+++ maniwebst/api/manifest.js
@@ -6,10 +6,9 @@ const openai = new OpenAI({
 
 export default async function handler(req, res) {
   // CORS headers
-  res.setHeader('Access-Control-Allow-Origin', 'https://maniwebst.com');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
   // Handle preflight request
   if (req.method === 'OPTIONS') {
